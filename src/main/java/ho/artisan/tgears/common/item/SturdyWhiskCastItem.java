61b7d3a8ed8682247fe32344ca5807dbbc8ca3f2
Chemiofitor4096 <hualongfentian123@outlook.com>
2025-09-04 20:30:49 +0800

    0.2.1 fix

--- src/main/java/ho/artisan/tgears/common/item/SturdyWhiskCastItem.java
+++ src/main/java/ho/artisan/tgears/common/item/SturdyWhiskCastItem.java
@@ -1,13 +1,13 @@
 package ho.artisan.tgears.common.item;
 
-import fr.lucreeper74.createmetallurgy.CreateMetallurgy;
 import ho.artisan.tgears.TinkersGears;
 import ho.artisan.tgears.util.RegistryUtil;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class SturdyWhiskCastItem extends WhiskCastItem implements CompatItem {
     public SturdyWhiskCastItem(Properties properties) {
-        super(RegistryUtil.find(ForgeRegistries.ITEMS, CreateMetallurgy.genRL("sturdy_whisk")), properties);
+        super(RegistryUtil.find(ForgeRegistries.ITEMS, new ResourceLocation("createmetallurgy", "sturdy_whisk")), properties);
     }
 
     @Override
